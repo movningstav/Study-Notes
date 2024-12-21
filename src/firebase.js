4e8998d4395575c40d5ef2570397d4a825311672
Hasan ELBİR <helbir15@gmail.com>
2024-12-21 17:04:38 +0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// src/firebase.js
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF9DHivUkkAAsS5S17LNijnuHMTv1JBLM",
+  authDomain: "studynotes42726.firebaseapp.com",
+  projectId: "studynotes42726",
+  storageBucket: "studynotes42726.firebasestorage.app",
+  messagingSenderId: "253881643882",
+  appId: "1:253881643882:web:d43a08ddaeab5affb3ab13",
+  measurementId: "G-9RTGQLV9SZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
