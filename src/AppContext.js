958a9df1fefbb37cc203b1d87a412d4eaadbd912
Hasan ELBİR <helbir15@gmail.com>
2024-12-21 18:06:44 +0300

    Update AppContext.js

--- src/AppContext.js
+++ src/AppContext.js
@@ -73,7 +73,7 @@ export const AppProvider = ({ children }) => {
     }
   };
 
-  // 4. Add a New Title (Optional: If you need to add new titles)
+  // 4. Add a New Title (Optional: If you need to add new titles dynamically)
   const addTitle = async (title, context) => {
     try {
       const newCategory = {
